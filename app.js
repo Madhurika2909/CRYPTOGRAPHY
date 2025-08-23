64e81366bb654d3eb8786a7bfac27f1e927fe442
Madhurika2909 <mpriya2909@gmail.com>
2025-08-24 00:24:48 +0530

    changes in app.js Serve static files from /public

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ server.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html on /
 server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 mongoose.connect(process.env.MONGO_URI);
